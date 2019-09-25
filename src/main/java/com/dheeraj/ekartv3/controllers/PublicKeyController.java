21980573f10dcd190fc8f19b0ca427d351bf54c8
Dheeraj Reddy <dheeraj@myboost.com.my>
2019-09-25 10:12:31 +0530

    Updated PublicKeyController to use Play Config Interface to get all the env variables rather than reading them directly using Sytem.getEnv method

--- src/main/java/com/dheeraj/ekartv3/controllers/PublicKeyController.java
+++ src/main/java/com/dheeraj/ekartv3/controllers/PublicKeyController.java
@@ -3,6 +3,8 @@ package com.dheeraj.ekartv3.controllers;
 import com.dheeraj.ekartv3.helper.Constant;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Preconditions;
+import com.google.inject.Inject;
+import com.typesafe.config.Config;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http;
@@ -17,12 +19,19 @@ public class PublicKeyController extends Controller {
     private String service;
     private String buildDate;
     private String version;
+    private Config config;
+
+    @Inject
+    public PublicKeyController(Config config) {
+        this.config = config;
+        service = Preconditions.checkNotNull(config.getString(Constant.SERVICE), "Service has to be set");
+        version = Preconditions.checkNotNull(config.getString(Constant.VERSION), "Version has to be set");
+        buildDate = Preconditions.checkNotNull(config.getString(Constant.BUILD_DATE), "build date has to be set");
+        publicKey = Preconditions.checkNotNull(config.getString(Constant.PUBLIC_KEY), "Public key has to set");
+    }
 
     public PublicKeyController() {
-        service = Preconditions.checkNotNull(getEnvironmentVariable(Constant.SERVICE), "Service has to be set");
-        version = Preconditions.checkNotNull(getEnvironmentVariable(Constant.VERSION), "Version has to be set");
-        buildDate = Preconditions.checkNotNull(getEnvironmentVariable(Constant.BUILD_DATE), "build date has to be set");
-        publicKey = Preconditions.checkNotNull(getEnvironmentVariable(Constant.PUBLIC_KEY), "Public key has to set");
+
     }
 
     public Result aboutMe(Http.Request request) {
