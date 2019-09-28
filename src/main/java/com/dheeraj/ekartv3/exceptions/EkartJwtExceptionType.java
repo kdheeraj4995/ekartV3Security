0704d018bbc82e3605275a62331480b402aefc23
Dheeraj Reddy <kdheeraj4995@gmail.com>
2019-09-28 08:22:40 +0530

    Created custom exceptions for serverToken verification

---
+++ src/main/java/com/dheeraj/ekartv3/exceptions/EkartJwtExceptionType.java
@@ -0,0 +1,11 @@
+package com.dheeraj.ekartv3.exceptions;
+
+/**
+ * @author Dheeraj Reddy
+ */
+public enum EkartJwtExceptionType {
+    EXPIRED_JWT_EXCEPTION,
+    MALFORMED_JWT_EXCEPTION,
+    JWT_SIGNATURE_EXCEPTION,
+    JWT_EXCEPTION
+}
--- src/main/java/com/dheeraj/ekartv3/util/JwtHelper.java
+++ src/main/java/com/dheeraj/ekartv3/util/JwtHelper.java
@@ -1,5 +1,7 @@
 package com.dheeraj.ekartv3.util;
 
+import com.dheeraj.ekartv3.exceptions.EkartJwtException;
+import com.dheeraj.ekartv3.exceptions.EkartJwtExceptionType;
 import com.google.common.base.Preconditions;
 import io.jsonwebtoken.*;
 
@@ -15,19 +17,19 @@ public class JwtHelper {
                 .compact();
     }
 
-    public static void validateJwtToken(String jwt, String key) {
+    public static void validateJwtToken(String jwt, String key) throws EkartJwtException {
         try {
             Jwts.parser()
                     .setSigningKey(key)
                     .parseClaimsJws(jwt);
         } catch (ExpiredJwtException e) {
-            throw e;
+            throw new EkartJwtException(EkartJwtExceptionType.EXPIRED_JWT_EXCEPTION);
         } catch (MalformedJwtException e) {
-            throw e;
+            throw new EkartJwtException(EkartJwtExceptionType.MALFORMED_JWT_EXCEPTION);
         } catch (SignatureException e) {
-            throw e;
+            throw new EkartJwtException(EkartJwtExceptionType.JWT_SIGNATURE_EXCEPTION);
         } catch (JwtException e) {
-            throw e;
+            throw new EkartJwtException(EkartJwtExceptionType.JWT_EXCEPTION);
         }
     }
 
